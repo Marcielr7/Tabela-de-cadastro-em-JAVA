f0180054c86797734519063239bc2303539b8466
Marcielr7 <marciel3389@gmail.com>
2023-02-18 18:33:05 -0300

    first commit

---
+++ src/main/java/com/mycompany/editando/Editando.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.editando;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * 
+ */
+public class Editando {
+    
+    
+ // parei aqui
+    public static void main(String[] args) {
+      File file = new File("C:\\arquivo.csv");
+      Scanner sc= null;
+     try{
+          sc = new Scanner(file);
+     }catch(IOException e){
+        
+      
+    }}
+}
